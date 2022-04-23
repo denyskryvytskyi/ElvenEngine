f131e78988990aa604c59841ac56abf4465b4220
Denys Kryvytskyi <kryvytskyi.denys@gmail.com>
2022-04-23 21:16:06 +0300

    Improved configure scripts

--- scripts/configure-win-vs2019.bat
+++ scripts/configure-win-vs2019.bat
@@ -1,7 +1,5 @@
 @echo off
 pushd %~dp0\..\
-mkdir build
-cd build
-call cmake .. -G "Visual Studio 16 2019" -A x64
+call cmake -B build -G "Visual Studio 16 2019" -A x64
 popd
 PAUSE
--- scripts/configure-win-vs2022.bat
+++ scripts/configure-win-vs2022.bat
@@ -1,7 +1,5 @@
 @echo off
 pushd %~dp0\..\
-mkdir build
-cd build
-call cmake .. -G "Visual Studio 17 2022" -A x64
+call cmake -B build -G "Visual Studio 17 2022" -A x64
 popd
 PAUSE
